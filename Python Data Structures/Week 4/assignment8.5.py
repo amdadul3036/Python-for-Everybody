fe119482a00854331877035f09b0eeb6edfa156e
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-10-05 20:47:50 -0700

    Week 4 assignment 8.5

---
+++ Python Data Structures/Week 4/assignment8.5.py
@@ -0,0 +1,12 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+fh = open(fname)
+count = 0
+data=[]
+for each in fh:
+    if each.startswith("From") and len(each.split())>2:
+        temp=each.split()
+        data.append(temp[1])
+for each in data:
+    print(each)
+print("There were", len(data), "lines in the file with From as the first word")
